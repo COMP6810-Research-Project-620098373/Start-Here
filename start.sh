cacc4218c7bc07e729ef80cfec1b25de5b15c33b
otboss <otsurfer6@gmail.com>
2023-04-13 21:13:06 -0500

    included system dependency check

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# Required System Dependencies: docker, docker-compose, git
+system_dependencies_required=("docker" "docker-compose" "git")
+
+for dep in ${system_dependencies_required[@]}; do
+    which $dep > /dev/null
+    if [ "$?" != "0" ]; then
+        printf "System dependency '$dep' is missing. Please install $dep and try again\n"
+        exit
+    fi
+done
 
 printf "\nDownloading Resources..."
 git clone git@github.com:COMP6810-Research-Project-620098373/Marketplace-Backend.git &> /dev/null
